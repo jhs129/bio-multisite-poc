f98a9489e20370df4dd1d78fdcbd1a20d04746d2
jhs129 <john.h.schneider@gmail.com>
2024-11-20 13:29:46 -0500

    if condition in middleware

--- middleware.ts
+++ middleware.ts
@@ -4,12 +4,16 @@ import type { NextRequest } from 'next/server';
 export function middleware(req: NextRequest) {
   const host = req.nextUrl.hostname;
 
-  if (host.startsWith('s1')) {
-    // Rewrite requests to s1-bio-multisite-poc.vercel.app to /site1
-    req.nextUrl.pathname = `/site1${req.nextUrl.pathname}`;
-  } else if (host.startsWith('s2')) {
-    // Rewrite requests to s2-bio-multisite-poc.vercel.app to /site2
-    req.nextUrl.pathname = `/site2${req.nextUrl.pathname}`;
+
+
+  if (!req.nextUrl.pathname.startsWith('/styles')) {
+    if (host.startsWith('s1')) {
+      // Rewrite requests to s1-bio-multisite-poc.vercel.app to /site1
+      req.nextUrl.pathname = `/site1${req.nextUrl.pathname}`;
+    } else if (host.startsWith('s2')) {
+      // Rewrite requests to s2-bio-multisite-poc.vercel.app to /site2
+      req.nextUrl.pathname = `/site2${req.nextUrl.pathname}`;
+    }
   }
 
   console.log('Middleware Path Rewrite:', req.nextUrl.pathname);
