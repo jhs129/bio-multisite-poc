85a1796b818d74b3338ef641b2085be8092739ca
jhs129 <john.h.schneider@gmail.com>
2024-11-20 09:54:03 -0500

    added middleware

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const host = req.nextUrl.hostname;
+
+  if (host.startsWith('s1')) {
+    // Rewrite requests to s1-bio-multisite-poc.vercel.app to /site1
+    req.nextUrl.pathname = `/site1${req.nextUrl.pathname}`;
+  } else if (host.startsWith('s2')) {
+    // Rewrite requests to s2-bio-multisite-poc.vercel.app to /site2
+    req.nextUrl.pathname = `/site2${req.nextUrl.pathname}`;
+  }
+
+  return NextResponse.rewrite(req.nextUrl);
+}
+
+export const config = {
+  matcher: '/:path*',
+};
